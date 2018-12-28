b89808de7cc34ca457bbf7868f5d7406d5a6aa9a
mramshaw <mramshaw@alumni.concordia.ca>
2018-12-27 23:41:19 -0500

    Add model prediction

--- iris.py
+++ iris.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+"""Quick ML with SciPy exercise."""
 
 # Load libraries
 
@@ -9,7 +10,7 @@ from pandas.plotting import scatter_matrix
 
 import seaborn as sb
 
-from sklearn import model_selection
+from sklearn import model_selection as ms
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
@@ -49,7 +50,8 @@ print
 # 1) - Univariate plots
 
 # Plot box-and-whisker (univariate) plots
-dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+dataset.plot(kind='box', subplots=True, layout=(2, 2),
+             sharex=False, sharey=False)
 plt.show()
 
 # Plot histograms
@@ -70,19 +72,23 @@ plt.show()
 
 # Segment our dataset into training and validation sets
 array = dataset.values
-X = array[:,0:4]
-Y = array[:,4]
-validation_size = 0.20 # 20 percent => Implies training data is 80 percent
-validation_seed = 7    # Using a seed allows us to compare subsequent runs
-X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size = validation_size, random_state = validation_seed)
+X = array[:, 0:4]
+Y = array[:, 4]
+validation_size = 0.20  # 20 percent => Implies training data is 80 percent
+validation_seed = 7     # Using a seed allows us to compare subsequent runs
+X_train, X_validation, Y_train, Y_validation = ms.train_test_split(
+    X, Y,
+    test_size=validation_size,
+    random_state=validation_seed)
 
 # Test option and evaluation metric
-test_seed = 7 # Can be different from the previous seed
+test_seed = 7  # Can be different from the previous seed
 scoring = 'accuracy'
 
 # Spot Check Algorithms
 models = []
-models.append(('LR', LogisticRegression(solver='liblinear', multi_class='auto')))
+models.append(('LR', LogisticRegression(solver='liblinear',
+                                        multi_class='auto')))
 models.append(('LDA', LinearDiscriminantAnalysis()))
 models.append(('KNN', KNeighborsClassifier()))
 models.append(('CART', DecisionTreeClassifier()))
@@ -93,12 +99,13 @@ models.append(('SVM', SVC(gamma='scale')))
 results = []
 names = []
 for name, model in models:
-	kfold = model_selection.KFold(n_splits = 10, random_state = test_seed)
-	cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv = kfold, scoring = scoring)
-	results.append(cv_results)
-	names.append(name)
-	msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-	print(msg)
+    kfold = ms.KFold(n_splits=10, random_state=test_seed)
+    cv_results = ms.cross_val_score(
+        model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
 
 """
 According to the tutorial, results should be as follows:
@@ -110,8 +117,45 @@ CART: 0.975000 (0.038188)
 NB: 0.975000 (0.053359)
 SVM: 0.981667 (0.025000)
 
-Along with some deprecation warnings, only SVM differed.
+Along with some deprecation warnings, only CART and SVM differed.
 
 I got:
+CART: 0.966667 (0.040825)
 SVM: 0.991667 (0.025000)
+"""
+
+# ---- Make predictions ----
+
+knn = KNeighborsClassifier()
+knn.fit(X_train, Y_train)
+predictions = knn.predict(X_validation)
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
+
+"""
+According to the tutorial, results should be as follows:
+
+0.9
+
+[[ 7  0  0]
+ [ 0 11  1]
+ [ 0  2  9]]
+
+             precision    recall  f1-score   support
+
+Iris-setosa       1.00      1.00      1.00         7
+Iris-versicolor   0.85      0.92      0.88        12
+Iris-virginica    0.90      0.82      0.86        11
+
+avg / total       0.90      0.90      0.90        30
+
+Instead of the final "avg / total" line I got the following:
+
+      micro avg       0.90      0.90      0.90        30
+      macro avg       0.92      0.91      0.91        30
+   weighted avg       0.90      0.90      0.90        30
+
+[Seems to be the same apart from the "macro avg".]
+
 """
